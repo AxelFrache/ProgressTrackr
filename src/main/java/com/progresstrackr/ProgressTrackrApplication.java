f3573431b99b2011f4156bc946b9eae543645001
frachea <axel.frache@etu.umontpellier.fr>
2023-05-30 01:51:33 +0500

    Mise en place du projet

---
+++ src/main/java/com/progresstrackr/ProgressTrackrApplication.java
@@ -0,0 +1,13 @@
+package com.progresstrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressTrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressTrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progresstrackr/ProgressTrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progresstrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressTrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
